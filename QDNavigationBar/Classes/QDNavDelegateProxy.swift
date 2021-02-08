a1f54ace940c659d55897a275e098f41ea916781
Sinno <sinno93@qq.com>
2021-02-08 23:36:20 +0800

    优化inject代码

--- QDNavigationBar/Classes/QDNavDelegateProxy.swift
+++ QDNavigationBar/Classes/QDNavDelegateProxy.swift
@@ -7,14 +7,8 @@
 
 import UIKit
 
-class QDNavDelegateProxySuper: NSObject, UINavigationControllerDelegate {
-   @objc dynamic func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
-        print("hellofdasfafasf")
-    }
-    
-}
 
-class QDNavDelegateProxy: QDNavDelegateProxySuper {
+class QDNavDelegateProxy: NSObject, UINavigationControllerDelegate {
     static var injectedDict: Set<String> = []
     // 为navDelegate注入方法
     static func inject(with navDelegate: AnyClass) {
@@ -26,8 +20,7 @@ class QDNavDelegateProxy: QDNavDelegateProxySuper {
         injectedDict.insert(classname)
         
         let originSel = #selector(navigationController(_:willShow:animated:))
-        let originMethod = class_getInstanceMethod(delegateClass, originSel)
-        if originMethod == nil {
+        if class_getInstanceMethod(delegateClass, originSel) == nil {
             // 该类没有实现originSel, 为其添加originSel，对应的实现为qdadd_navigationController
             let addsel = #selector(qdadd_navigationController(_:willShow:animated:))
             let addmethod = class_getInstanceMethod(self, addsel)
@@ -51,7 +44,7 @@ class QDNavDelegateProxy: QDNavDelegateProxySuper {
         }
        
         let replaceSel = #selector(qdreplace_navigationController(_:willShow:animated:))
-        guard let noimp_addmethod = class_getInstanceMethod(self, originSel), let selfReplaceMethod = class_getInstanceMethod(self, replaceSel)  else {
+        guard let selfReplaceMethod = class_getInstanceMethod(self, replaceSel)  else {
             assert(false)
             return
         }
@@ -84,25 +77,11 @@ class QDNavDelegateProxy: QDNavDelegateProxySuper {
             return
         }
         
-        // 要判断是当前类还是父类实现：
-        // 尝试添加originSel, 如果当前类(而非父类)已经实现，则会添加不成功，此时直接交换方法即可
-        // 若当前未实现，则我们给当前类加上了一个实现，也直接交换即可
-        let addOriSuccess = class_addMethod(delegateClass, originSel, method_getImplementation(noimp_addmethod), method_getTypeEncoding(noimp_addmethod))
-        if addOriSuccess {
-            // 添加成功，说明该类本身没有实现，现在我们已经为它加了一个实现，直接交换
-            guard let originMethod = class_getInstanceMethod(delegateClass, originSel), let replaceMethod = class_getInstanceMethod(delegateClass, delegateReplaceSel) else {
-                assert(false)
-                return
-            }
-            method_exchangeImplementations(originMethod, replaceMethod)
-        } else {
-            // 添加失败，说明该类本身已经实现，我们也可以直接交换
-            guard let originMethod = class_getInstanceMethod(delegateClass, originSel), let replaceMethod = class_getInstanceMethod(delegateClass, delegateReplaceSel) else {
-                assert(false)
-                return
-            }
-            method_exchangeImplementations(originMethod, replaceMethod)
+        guard let originMethod = class_getInstanceMethod(delegateClass, originSel), let replaceMethod = class_getInstanceMethod(delegateClass, delegateReplaceSel) else {
+            assert(false)
+            return
         }
+        method_exchangeImplementations(originMethod, replaceMethod)
         
     }
     // 在原代理没有实现时，添加此方法
@@ -112,12 +91,9 @@ class QDNavDelegateProxy: QDNavDelegateProxySuper {
         }
     }
     
-    // 在原代理仅父类实现时，添加此方法
-    @objc dynamic override func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
-        // 调用super
-        if super.responds(to: #selector(navigationController(_:willShow:animated:))) {
-            super.navigationController(navigationController, willShow: viewController, animated: animated)
-        }
+    // 仅用于占位，避免警告和报错
+    @objc dynamic func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
+        
     }
     
     // 在原代理有实现时，使用此方法替换
