282343ede5468d14ba0fcda434b706a81f6cbc95
sinno <sinno93@qq.com>
2021-02-19 00:35:10 +0800

    修改深色模式demo vc

--- Example/QDNavigationBar/specialCase/DarkModeTestViewController.swift
+++ Example/QDNavigationBar/specialCase/DarkModeTestViewController.swift
@@ -10,11 +10,11 @@ import UIKit
 import QDNavigationBar
 
 class DarkModeTestViewController: UITableViewController {
-
+    var tipTitle = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Dark Mode Test"
-        self.view.backgroundColor = UIColor.lightGray
+        self.view.backgroundColor = UIColor.white
         if #available(iOS 13.0, *) {
             let config = self.navigationController?.navBarConfig?.copy() as? QDNavigationBarConfig
             config?.backgroundColor = UIColor(dynamicProvider: { (traitEnv) -> UIColor in
@@ -23,12 +23,39 @@ class DarkModeTestViewController: UITableViewController {
                 }
                 return UIColor.white
             })
+            self.tipTitle = "打开/关闭Dark Mode，查看导航栏效果"
             self.navBarConfig = config
         } else {
-            
+            let config = self.navigationController?.navBarConfig?.copy() as? QDNavigationBarConfig
+            config?.transitionStyle = .none
+            self.navBarConfig = config
+            self.tipTitle = "iOS13.0以下系统不支持设置Dark Mode"
         }
         // Do any additional setup after loading the view.
     }
-    
 
+}
+
+// MARK: - Delegates
+// 实现遵循的代理方法
+extension DarkModeTestViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId") ?? UITableViewCell(style: .default, reuseIdentifier: "cellId")
+        if indexPath.row == 0 {
+            cell.textLabel?.text = tipTitle
+        } else {
+            cell.textLabel?.text = ""
+        }
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView .deselectRow(at: indexPath, animated: true)
+    }
+    
 }
