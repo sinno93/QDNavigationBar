eaefa4d495250ab21ea4bf97965e1615a20e3c81
sinno <sinno93@qq.com>
2021-06-15 13:07:55 +0800

    test

--- Example/QDNavigationBar/SampleListViewController.swift
+++ Example/QDNavigationBar/SampleListViewController.swift
@@ -14,6 +14,7 @@ class SampleListViewController: UIViewController{
     // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        // test
         self.title = "Special Case"
         
         // 为导航控制器开启QDNavigationBar样式管理功能
